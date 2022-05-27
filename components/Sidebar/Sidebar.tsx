4b892a71ea75c94deaf6f7d9a60faae1be1df281
maltael <elena.malta@gellify.com>
2022-05-27 10:59:52 +0200

    fix lint

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import styles from './sidebar.module.scss'
+import Link from "next/link";
+import styles from "./sidebar.module.scss";
 
 export default function Sidebar() {
-    return (
-        <nav className={styles.nav}>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/dashboard">
-                <a>About</a>
-            </Link>
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/dashboard">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
 }
